cb9c2c4dd0cfa9c7edf2d9e245fa8bc365bb24e5
Guilherme Sartori <gui.sartori96@gmail.com>
2020-07-10 23:39:32 -0300

    agora o sistema recebe dinamicamente as funcionalidades novas ou se selecionada uma já existente

--- src/controllers/CallController.ts
+++ src/controllers/CallController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import { Connection } from "typeorm"
 import path from 'path'
 import { Call } from "../entity/Call"
+import { Functionality } from "../entity/Functionality"
 
 class CallController {
 
@@ -12,6 +13,9 @@ class CallController {
     ) => {
         const { projectId } = req.params
         const repo = conn.getRepository(Call)
+
+
+
         try {
 
             const calls = await repo
@@ -45,13 +49,25 @@ class CallController {
         const {
             title,
             description,
-            projectId
+            projectId,
+            functionality
         } = req.body
+
+        let functionalityId = functionality.value
+
+        if (functionality.__isNew__) {
+            const newFunc = new Functionality()
+            newFunc.name = functionality.value
+            newFunc.project = projectId
+
+            functionalityId = await newFunc.save().then(func => func.id)
+        }
 
         const call = new Call()
         call.title = title
         call.description = description
         call.project = projectId
+        call.functionality = functionalityId
 
         try {
             call.save()
