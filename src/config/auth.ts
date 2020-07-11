2b6d5c6138aec709de7b8d4302267e8b2f25a5fb
Guilherme Sartori <gui.sartori96@gmail.com>
2020-07-11 03:21:28 -0300

    trabalhando na auth.ts para evitar erros a curto prazo

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -7,34 +7,34 @@ import UserController from '../controllers/UserController'
 const LocalStrategy = passportLocal.Strategy
 
 const auth = (passport: PassportStatic) => {
-    
-    //TODO: FAZER TRATAMETO DE TODAS AS POSSIBILIDADES DE ERRO
-    passport.serializeUser((user: User, done) => {
-        done(null, user.id)
-    })
 
-    passport.deserializeUser((id: number, done) => {
-        UserController.findUserById(id, (data: User) => {
-            done(null, data)
-        })
-    })
+    // //TODO: FAZER TRATAMETO DE TODAS AS POSSIBILIDADES DE ERRO
+    // passport.serializeUser((user: User, done) => {
+    //     done(null, user.id)
+    // })
 
-    passport.use(new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password'
-    }, (email, password, done) => {
-        UserController.findUser(email, user => {
-            if(!user){
-                return done(null, false)
-            }
+    // passport.deserializeUser((id: number, done) => {
+    //     UserController.findUserById(id, (data: User) => {
+    //         done(null, data)
+    //     })
+    // })
 
-            bcrypt.compare(password, String(user.password), (error, isValid) => {
-                if (error) return done(error)
-                if (!isValid) return done(null, false)
-                return done(null, user)
-            })
-        })
-    }))
+    // passport.use(new LocalStrategy({
+    //     usernameField: 'email',
+    //     passwordField: 'password'
+    // }, (email, password, done) => {
+    //     UserController.findUser(email, user => {
+    //         if(!user){
+    //             return done(null, false)
+    //         }
+
+    //         bcrypt.compare(password, String(user.password), (error, isValid) => {
+    //             if (error) return done(error)
+    //             if (!isValid) return done(null, false)
+    //             return done(null, user)
+    //         })
+    //     })
+    // }))
 }
 
 export default auth
